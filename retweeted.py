6624f09d814cc906451978434b40fad801419905
acastillo20 <acastio@uc.cl>
2022-08-17 18:12:27 -0400

    Added functionality top_10_retweet

--- retweeted.py
+++ retweeted.py
@@ -1,9 +1,6 @@
 import json
-import retweeted
 
 data = [json.loads(line) for line in open("farmers-protest-tweets-2021-03-5.json", "r", encoding="utf-8")]
 
-def main(accion):
-
-    if accion == "top_10_retweet":
-        return retweeted.top_10_retweet(data)
+def top_10_retweet(data):
+    return sorted(data, key=lambda k: k["retweetCount"], reverse=True)[:10]
