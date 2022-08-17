68fce55c28f9b2cf6ccf081d6847d0f776711fe4
acastillo20 <acastio@uc.cl>
2022-08-17 18:13:34 -0400

    small fixes

--- retweeted.py
+++ retweeted.py
@@ -1,6 +1,2 @@
-import json
-
-data = [json.loads(line) for line in open("farmers-protest-tweets-2021-03-5.json", "r", encoding="utf-8")]
-
 def top_10_retweet(data):
     return sorted(data, key=lambda k: k["retweetCount"], reverse=True)[:10]
